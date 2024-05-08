59c61578991b77664a812c893141448d84ed766c
Mykola Rudenko <m.rudenko.rb@gmail.com>
2024-05-08 17:52:23 +0300

    add code style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true
+      },
+      colors: {
+        "almost-white": "hsl(0, 0%, 98%)",
+        "medium-gray": "hsl(0, 0%, 41%)",
+        "almost-black": "hsl(0, 0%, 8%)"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
